f9fb205e6e055b4877596f6cef9c47ac7b6e486f
c0d3r <latrenda32@debutqx.com>
2023-01-06 11:28:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://github.com/assc0d3r/ARTWZML ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://github.com/assc0d3r/ARTWZML ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 update.py && python3 -m bot &")
