2ea3a556311e5c980c9de3a440c9bac83c653ffd
c0d3r <latrenda32@debutqx.com>
2023-01-06 12:44:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://assc0d3r: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@github.com/assc0d3r/MOGENIUSWEEBZONE ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 update.py && python3 -m bot &")
+os.system("git https://assc0d3r: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@github.com/assc0d3r/MOGENIUSWEEBZONE ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 update.py && python3 -m bot &")
 
