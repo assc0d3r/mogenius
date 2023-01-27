415f6e033fc35e86919e9a04339af5700d04c6dc
c0d3r <latrenda32@debutqx.com>
2023-01-27 22:12:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://github.com/assc0d3r/ARTLEECHRENDERBYPASSS ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 update.py && python3 -m bot &")
+os.system("git clone https://github.com/assc0d3r/ARTLEECHRENDERBYPASSS")
 
