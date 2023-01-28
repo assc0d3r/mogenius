c786ba8de77bb8776ded2569c6ec091ae5fa9563
c0d3r <latrenda32@debutqx.com>
2023-01-28 11:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://github.com/assc0d3r/ARTLEECHRENDERBYPASSS ok && cd ok && docker")
+os.system("git clone https://github.com/assc0d3r/ARTLEECHRENDERBYPASSS ok && cd ok && docker run Docker")
 
